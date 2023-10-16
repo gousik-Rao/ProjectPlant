4db0134ae4edb9078735a59e8a5d9034cca09814
gousik_05_ <125201504+CapBlac@users.noreply.github.com>
2023-10-16 19:26:43 +0530

    First commit of all files

---
+++ script.js
@@ -0,0 +1,67 @@
+
+var sidebar = document.querySelector(".sidebar");
+var menubar = document.getElementById("bars");
+var closebars = document.getElementById("closenav");
+var body=document.getElementById("body");
+
+sidebar.style.right="-600px";
+
+menubar.addEventListener("click", function () {
+    sidebar.style.right = "0px";
+});
+
+closebars.addEventListener("click", function () {
+    sidebar.style.right = "-600px";
+  
+});
+
+
+    var news=document.getElementById("news");
+    var popup=document.getElementById("popup");
+    var button1=document.querySelector("btn1");
+    button1.addEventListener("click",function(e){
+        e.preventDefault();
+        if(news==true){
+            popup.textContent="Email entered Successfully";
+            popup.style.backgroundcolor="rgba(127, 255, 212, 0.485)";
+            popup.style.bottom="10px";
+            popup.style.opacity="0.7";
+        }
+        else{
+            popup.textContent="Enter email properly";
+            popup.style.backgroundcolor="red";
+            popup.style.opacity="0.7";
+            popup.style.bottom="10px";
+        }
+
+        setTimeout(function(){ 
+            popup.style.bottom="-40px";
+            popup.style.opacity="0";
+        },2000 )
+
+    })
+
+    var box=document.querySelector(".box");
+    var destn=document.querySelector(".destn");
+    var seller=document.querySelector(".seller");
+
+    window.addEventListener("scroll",function(){
+        var windowheight=window.innerHeight;
+        var boxtop=box.getBoundingClientRect().top;
+        
+        if(windowheight>boxtop){
+            box.style.opacity="1";
+            destn.style.opacity="1";
+            seller.style.opacity="1";
+        }
+        else{
+            box.style.opacity="0";
+            destn.style.opacity="0";
+            seller.style.opacity="0";
+            // box.style.opacity="0";
+
+        }
+        
+    
+    })
+
