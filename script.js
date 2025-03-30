2dba9d983a48ccca69749b41c92c7fa87cbcef84
Gousik <gousikthason@gmail.com>
2025-03-30 15:48:19 +0530

    New appearance changes

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-var newsFill=false;
-var sidebar = document.querySelector(".sidebar");
-var menubar = document.getElementById("bars");
-var closebars = document.getElementById("closenav");
-var body = document.getElementById("body");
+let newsFill = false;
+let sidebar = document.querySelector(".sidebar");
+let menubar = document.getElementById("bars");
+let closebars = document.getElementById("closenav");
+let body = document.getElementById("body");
+
+let newsLetter = document.querySelector(".newsletter");
+let icon = newsLetter.querySelector("i");
+let news = document.querySelector(".news");
+let mailLabel = newsLetter.querySelector("label");
+
+// Method to display the menu bar when the screen size reduces below 901 px
+function toggleNavBar() {
+    if (window.innerWidth < 1101) {
+        menubar.style.display = "block";
+    } else {
+        menubar.style.display = "none";
+        sidebar.style.right = "-60%"; // Ensure sidebar is hidden
+    }
+}
+toggleNavBar();
+
+window.addEventListener("resize", toggleNavBar);
 
 menubar.addEventListener("click", function (e) {
     e.preventDefault();
@@ -10,87 +28,68 @@ menubar.addEventListener("click", function (e) {
 });
 
 closebars.addEventListener("click", function (e) {
-    // e.preventDefault();
-    sidebar.style.right = "-600px";
-  
+    e.preventDefault();
+    sidebar.style.right = "-60%";
 });
 
+news.addEventListener("keyup", function() {
+    let enteredValue = news.value;
 
-var newsLetter=document.querySelector(".newsletter");
-var icon=newsLetter.querySelector("i");
-var news=document.querySelector(".news");
-var mailLabel=newsLetter.querySelector("label");
-
-news.addEventListener("keyup",function(e){
-    //user input value
-    var enteredValue=news.value;
-
-    if(enteredValue=="")
-    //hide icon and show label
-    {
-        icon.style.visibility="hidden";
-        mailLabel.style.visibility="visible";
-        newsFill=false;
+    if (enteredValue == "") {
+        icon.style.visibility = "hidden";
+        mailLabel.style.visibility = "visible";
+        newsFill = false;
+    } else {
+        icon.style.visibility = "visible";
+        mailLabel.style.visibility = "hidden";
+        newsFill = true;
     }
-    else
-    //show icon and hide label
-    {
-        icon.style.visibility="visible";
-        mailLabel.style.visibility="hidden";
-        newsFill=true;
-    }
-})
-
-
-var popup=document.querySelector(".popup");
-var button1=document.querySelector(".btn1");
-
-button1.addEventListener("click",function(e){
-        //prevent default execution
-        e.preventDefault();
-        if(newsFill==true){
-            popup.textContent="Subscription Successful";
-            popup.style.backgroundColor="black";
-        }
-        else{
-            popup.textContent="Enter valid email";
-            popup.style.backgroundColor="red";
-            
-        }
-        popup.style.bottom="10%";
-        popup.style.opacity="1";
-
-        //run line after 2 seconds
-        //set timeout (What should happen, after how many  milliseconds)
-        setTimeout(function(){ 
-            popup.style.bottom="-50%";
-            popup.style.opacity="0";
-        },2500 )
-
 });
 
+let popup = document.querySelector(".popup");
+let button1 = document.querySelector(".btn1");
 
-var box=document.querySelector(".box");
-var destn=document.querySelector(".destn");
-var seller=document.querySelector(".seller");
-
-    window.addEventListener("scroll",function(){
-        var windowheight=window.innerHeight;
-        var boxtop=box.getBoundingClientRect().top;
-        
-        if(windowheight > boxtop){
-            box.style.opacity="1";
-            destn.style.opacity="1";
-            seller.style.opacity="1";
-        }
-        else{
-            box.style.opacity="0";
-            destn.style.opacity="0";
-            seller.style.opacity="0";
-            // box.style.opacity="0";
-
-        }
-        
+button1.addEventListener("click", function (e) {
     
-})
+    if (newsFill === true) {
+        popup.textContent = "Subscription Successful";
+        popup.style.backgroundColor = "#F2CFF8";
+		popup.style.color = "#285B23"
+        popup.style.top = "2%";
+        popup.style.opacity = "1";
+		
+    } else if(newsFill === false){
+		e.preventDefault();
+		popup.style.color = "#F2CFF8"
+        popup.textContent = "Enter e-mail ID";
+        popup.style.backgroundColor = "red";
+        popup.style.top = "2%";
+        popup.style.opacity = "1";
+    }
 
+    //run line after 2 seconds
+    //set timeout (What should happen, after how many  milliseconds)
+    setTimeout(function () {
+        popup.style.top = "-50%";
+        popup.style.opacity = "0";
+    }, 3000)
+});
+
+let box = document.querySelector(".boxes");
+let destn = document.querySelector(".destn");
+let seller = document.querySelector(".seller");
+
+window.addEventListener("scroll", function () {
+    let windowHeight = window.innerHeight;
+    let boxTop = box.getBoundingClientRect().top;
+
+    if (windowHeight > boxTop) {
+        box.style.opacity = "1";
+        destn.style.opacity = "1";
+        seller.style.opacity = "1";
+    } else {
+        box.style.opacity = "0";
+        destn.style.opacity = "0";
+        seller.style.opacity = "0";
+    }
+});
